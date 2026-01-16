7a94be40352fe66a88375d8730f10e6d231b0262
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2026-01-15 22:34:28 -0500

    added swerve with pose?

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,52 @@
 package frc.robot;
 
-public class Constants {
+import com.pathplanner.lib.config.PIDConstants;
+
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Rotation3d;
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
+
+public final class Constants {
+
+    public static final double ROBOT_MASS = Units.lbsToKilograms(120); // TODO: Update with correct weight
+    public static final Matter CHASSIS = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS); // TODO: Update with correct COG locations
+    public static final double LOOP_TIME = 0.13;
+    public static final double MAX_SPEED = Units.feetToMeters(15.1);
+ 
+    public static final class Swerve {
+        public static final class Heading {
+            public static final double TOLERANCE = 3; // In degrees
+
+            public static final PIDConstants HEADING_PID = new PIDConstants(0, 0, 0);
+        }
+
+        public static final class Translation {
+            public static final double TOLERANCE = Units.inchesToMeters(1.0);
+
+            public static final PIDConstants TRANSLATION_PID = new PIDConstants(0, 0, 0);
+        }
+    }
+
+    public static class Vision {
+        public static class CAMERA_PLACEHOLDER {
+            public static Rotation3d ORIENTATION = new Rotation3d(0, 0, 0);
+            public static Translation3d TRANSLATION = new Translation3d(0, 0, 0);
+            public static Matrix<N3, N1> SINGLE_TAG_STD_DEVS = VecBuilder.fill(1, 1, 4);
+            public static Matrix<N3, N1> MULTI_TAG_STD_DEVS = VecBuilder.fill(0.5, 0.5, 2);
+        }
+    }
     
+    public static class OperatorConstants {
+        // Joystick Deadband
+        public static final double DEADBAND = 0.0;
+        public static final double LEFT_Y_DEADBAND = 0.0;
+        public static final double RIGHT_X_DEADBAND = 0.0;
+        public static final double TURN_CONSTANT = 6;
+    }
 }
