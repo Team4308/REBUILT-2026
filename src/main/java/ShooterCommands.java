2a9740644dba2a9535342e0adf5035a78f3916cb
Arav <Aravraina666git config --global user.email Aravraina666@gmail.com>
2026-02-06 16:23:29 -0500

    I added so much code

---
+++ src/main/java/ShooterCommands.java
@@ -0,0 +1,17 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import co m.ctre.phoenix6.signals.NeutralModeValue;
+
+import ca.team4308.absolutelib.wrapper.LogSubsystem;
+import edu.wpi.first.util.sendable.Sendable;
+import frc.robot.Constants;
+
+public class ShooterCommands extends LogSubsystem{
+    
+}
