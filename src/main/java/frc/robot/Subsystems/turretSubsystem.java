d53cf76179f43f6353d34300898f2b1ba6244af9
Owen Sun <owen31474@gmail.com>
2026-02-10 16:31:59 -0500

    hello

--- src/main/java/frc/robot/Subsystems/turretSubsystem.java
+++ src/main/java/frc/robot/Subsystems/turretSubsystem.java
@@ -8,6 +8,7 @@ import org.opencv.core.Mat;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import ca.team4308.absolutelib.math.DoubleUtils;
 import ca.team4308.absolutelib.wrapper.AbsoluteSubsystem;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.util.sendable.Sendable;
@@ -19,17 +20,24 @@ public class turretSubsystem extends SubsystemBase {
 
     private final TalonFX turretMotor;
 
+    private final double CANCODER1_GEAR_RATIO = 85/31;
+    private final double CANCODER2_GEAR_RATIO = 85/33;
+    private final double DRIVE_MOTOR_GEAR_RATIO = 12/85;
+
+    public double targetAngle = 0.0;
+    private boolean isAtTarget = false;
+
     public turretSubsystem() {
         turretMotor = new TalonFX(0);
+
     }
 
     public double getTurretAngle() {
         return 0.0;
-        //placeholder for finding the actual turret angle
     }
 
     public boolean isAtTarget(double degrees) {
-    return Math.abs(MathUtil.inputModulus(degrees - getTurretAngle(), -180, 180)) < 1.0;
+        return Math.abs(MathUtil.inputModulus(degrees - getTurretAngle(), -180, 180)) < 1.0;
     }
 
     public void setTarget(double degrees) {
@@ -40,32 +48,29 @@ public class turretSubsystem extends SubsystemBase {
         } 
     }
 
-    public void resetTurret(double degrees) {
-        degrees = 0.0;
-        double error = degrees - getTurretAngle();
-        turretMotor.set(error * 0.01);
+    public Command moveToTarget(Supplier<Double> degrees) {
+        return run(() -> {
+            double currentTarget = degrees.get();
+            double error = currentTarget - getTurretAngle();
+            turretMotor.set(error * 0.01);
+        }).until(this::isAtTarget);
+    }
+    /* 
+    public Command moveToTarget(Supplier<Double> degrees, double timeoutMs) {
+
     }
 
-    //setTarget(); - Needs to be Uncommented
-    // boolean isAtTarget() {} // returns whether the turret degree is within x degrees of the target. (x to be put in constants.java)
+    public void resetTurret() {}
+    public Command resetTurret() {}
 
-    // Command moveToTarget(Supplier<Double> degrees) {} // runs a command that moves the turret to the target. It must end when the turret is at the correct position
+    public void aimAtHub() {}
+    public Command aimAtHub() {}
 
-    // Command moveToTarget(Supplier<Double> degrees, double timeoutMs) {} // similar to moveToTarget, but if it reaches the timeout (milliseconds) first, it must end no matter what.
+    public void aimAtPassingZone() {}
+    public Command aimAtPassingZone() {}
 
-    // void resetTurret() {} // moves back to starting position
+    public void setState(String state) {}
 
-    // Command resetTurret() {} // moves back to starting position, until it is at target
-
-    // void aimAtHub() {} // auto targets to hub - talk to nicholas on how to do this.
-    // Command aimAtHub() {} // same but its an command - it should not end, but rather run until interrupted
-
-    // void aimAtPassingZone() {} // aims towards the area we will pass too - specific location will be in strategy
-    // Command aimAtPassingZone() {} // same thing, it will not end until interrupted
-
-    // void setState(String state) {} // sets the current state
-
-    // void setStateBased(boolean using) {} // turns on/off the state manager
-
-
+    public void setStateBased(boolean using) {}
+    */
 }
