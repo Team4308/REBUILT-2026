02a0adb842b06051eca1c433a27332b7cace0ab5
Doodlelicious <owen31474@gmail.com>
2026-02-10 11:49:06 -0500

    Added to turret subsystem
    Nothing impactful. I legit have no idea how to move the turret, but I added the boolean for isattarget, and made a placeholder for getTurretAngle(). Also I don't think print should be used in this code.

--- src/main/java/frc/robot/Subsystems/turretSubsystem.java
+++ src/main/java/frc/robot/Subsystems/turretSubsystem.java
@@ -1,6 +1,15 @@
 package frc.robot.Subsystems;
 
+import static edu.wpi.first.units.Units.Degrees;
+
+import java.util.function.Supplier;
+
+import org.opencv.core.Mat;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
 import ca.team4308.absolutelib.wrapper.AbsoluteSubsystem;
+import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
@@ -8,21 +17,34 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class turretSubsystem extends SubsystemBase {
 
-
-    //double getTurretAngle() {} // returns rotation in degrees from 0-360
+    private final TalonFX turretMotor;
 
     public turretSubsystem() {
+        turretMotor = new TalonFX(0);
+    }
 
+    public double getTurretAngle() {
+        return 0.0;
+        //placeholder for finding the actual turret angle
+    }
+
+    public boolean isAtTarget(double degrees) {
+    return Math.abs(MathUtil.inputModulus(degrees - getTurretAngle(), -180, 180)) < 1.0;
     }
 
     public void setTarget(double degrees) {
         double targetAngle  = 0;
         if (degrees > 360) {
             targetAngle = degrees - (360 * Math.floor(degrees / 360));
-            System.out.println(targetAngle);
-        }    
+            System.out.println(targetAngle);//should be return targetAngle, no?
+        } 
     }
 
+    public void resetTurret(double degrees) {
+        degrees = 0.0;
+        double error = degrees - getTurretAngle();
+        turretMotor.set(error * 0.01);
+    }
 
     //setTarget(); - Needs to be Uncommented
     // boolean isAtTarget() {} // returns whether the turret degree is within x degrees of the target. (x to be put in constants.java)
