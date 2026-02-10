c6ecf8e640b68b2753f40382f71404e4e9f993b8
Stephen Kizel <Bananagod2000@gmail.com>
2026-02-10 16:42:33 -0500

    add mild change

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,10 +1,18 @@
 package frc.robot;
 
 public class Constants {
+    public static class Shooter {
+        public static final double maxrpm = 100;
+        public static final double topMultiplier = 1;
+        public static final double bottomMultiplier = 1;
+        public static final int kRPMTolerance = 10;
+        }
     public static class Mapping {
         public static class ShooterMotor {
         // Change ID later
         public static final int kMotor1 = 14;
         public static final int kMotor2 = 15;
+        public static final int kRPMTolerance = 10;
+        }
     }
 }
