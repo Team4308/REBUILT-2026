be9bc37fd1010f687fcc6e52a5bc742adcbdcd3d
snowman0607 <akhilmani0607@gmail.com>
2026-02-12 15:35:45 -0500

    hihih

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,44 @@
 package frc.robot;
 
-public class Constants {
-    
+public final class Constants {
+
+  public static final class Intake {
+
+    // CAN IDs
+    public static final int ROLLER_ID = 20;
+    public static final int PIVOT_ID = 21;
+
+    // Roller tuning
+    public static final double ROLLER_GEAR_RATIO = 1.0;
+    public static final double ROLLER_KP = 0.12;
+    public static final double ROLLER_KI = 0.0;
+    public static final double ROLLER_KD = 0.0;
+    public static final double ROLLER_KV = 0.12;
+
+    public static final double ROLLER_INTAKE_RPM = 4500.0;
+
+    // Pivot geometry
+    public static final double PIVOT_GEAR_RATIO = 100.0;
+    public static final double PIVOT_KP = 40.0;
+    public static final double PIVOT_KI = 0.0;
+    public static final double PIVOT_KD = 2.0;
+    public static final double PIVOT_KG = 0.35; // gravity feedforward
+    public static final double PIVOT_KV = 1.2;
+    public static final double PIVOT_KA = 0.08;
+
+    // Motion Magic
+    public static final double MAX_VEL_DEG_PER_SEC = 300.0;
+    public static final double MAX_ACCEL_DEG_PER_SEC2 = 600.0;
+
+    // Angles
+    public static final double RETRACTED_ANGLE_DEG = 0.0;
+    public static final double INTAKE_ANGLE_DEG = 65.0;
+
+    // Agitate
+    public static final double AGITATE_LOW_DEG = 45.0;
+    public static final double AGITATE_HIGH_DEG = 70.0;
+    public static final double ANGLE_TOLERANCE_DEG = 1.5;
+  }
 }
+
+
--- src/main/java/frc/robot/subsystems/intakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/intakeSubsystem.java
@@ -1,7 +1,94 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.configs.*;
 
-public class intakeSubsystem extends SubsystemBase {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IntakeSubsystem extends SubsystemBase {
+
+  private final TalonFX roller = new TalonFX(Constants.Intake.ROLLER_ID);
+  private final TalonFX pivot = new TalonFX(Constants.Intake.PIVOT_ID);
+
+  private final VelocityVoltage rollerRequest = new VelocityVoltage(0);
+  private final MotionMagicVoltage pivotRequest = new MotionMagicVoltage(0);
+
+  private double targetAngleDeg = Constants.Intake.RETRACTED_ANGLE_DEG;
+
+  public IntakeSubsystem() {
+    configureRoller();
+    configurePivot();
+
+    // Intake always starts retracted
+    pivot.setPosition(0);
+  }
+
+  /* ---------------- Roller ---------------- */
+
+  public void setRollerSpeed(double rpm) {
+    roller.setControl(
+        rollerRequest.withVelocity(rpm / 60.0));
+  }
+
+  public void stopRoller() {
+    roller.stopMotor();
+  }
+
+  /* ---------------- Pivot ---------------- */
+
+  public void setIntakeAngle(double angleDeg) {
+    targetAngleDeg = angleDeg;
+    pivot.setControl(
+        pivotRequest.withPosition(degToRot(angleDeg)));
+  }
+
+  public boolean isAtAngle() {
+    double currentDeg = rotToDeg(pivot.getPosition().getValueAsDouble());
+    return Math.abs(currentDeg - targetAngleDeg)
+        < Constants.Intake.ANGLE_TOLERANCE_DEG;
+  }
+
+  /* ---------------- Helpers ---------------- */
+
+  private void configureRoller() {
+    TalonFXConfiguration cfg = new TalonFXConfiguration();
+
+    cfg.Slot0.kP = Constants.Intake.ROLLER_KP;
+    cfg.Slot0.kI = Constants.Intake.ROLLER_KI;
+    cfg.Slot0.kD = Constants.Intake.ROLLER_KD;
+    cfg.Slot0.kV = Constants.Intake.ROLLER_KV;
+
+    roller.getConfigurator().apply(cfg);
+  }
+
+  private void configurePivot() {
+    TalonFXConfiguration cfg = new TalonFXConfiguration();
+
+    cfg.Slot0.kP = Constants.Intake.PIVOT_KP;
+    cfg.Slot0.kI = Constants.Intake.PIVOT_KI;
+    cfg.Slot0.kD = Constants.Intake.PIVOT_KD;
+    cfg.Slot0.kG = Constants.Intake.PIVOT_KG;
+    cfg.Slot0.kV = Constants.Intake.PIVOT_KV;
+    cfg.Slot0.kA = Constants.Intake.PIVOT_KA;
+
+    cfg.MotionMagic.MotionMagicCruiseVelocity =
+        degToRot(Constants.Intake.MAX_VEL_DEG_PER_SEC);
+    cfg.MotionMagic.MotionMagicAcceleration =
+        degToRot(Constants.Intake.MAX_ACCEL_DEG_PER_SEC2);
+
+    pivot.getConfigurator().apply(cfg);
+  }
+
+  private double degToRot(double deg) {
+    return deg / 360.0 * Constants.Intake.PIVOT_GEAR_RATIO;
+  }
+
+  private double rotToDeg(double rot) {
+    return rot / Constants.Intake.PIVOT_GEAR_RATIO * 360.0;
+  }
 }
+
+
