f4f2654dd2359d3fb974f4e7e0ffdb9338e8128b
Ronav Shar <rjmakes2020@gmail.com>
2026-02-12 16:15:46 -0500

    added state commands and stopmotor commands

--- src/main/java/frc/robot/Climbersubsystem.java
+++ src/main/java/frc/robot/Climbersubsystem.java
@@ -6,18 +6,16 @@ import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Climbersubsystem extends SubsystemBase {
 
-    private final TalonFX motor1 = new TalonFX(1);  // Leader
-    private final TalonFX motor2 = new TalonFX(2); // Follower
+    private final TalonFX motor1 = new TalonFX(Constants.Motor1ID);  // Leader
+    private final TalonFX motor2 = new TalonFX(Constants.Motor2ID); // Follower
 
-    public double Revolutions = 15.0;
     public double StartingPos = 0;
-    public double Tolerance = 5;
-    public double Fullextend = 20;
-
+    
     private final PositionVoltage positionRequest = new PositionVoltage(0);
 
     public Climbersubsystem() {
@@ -31,23 +29,36 @@ public class Climbersubsystem extends SubsystemBase {
 
         // Make TalonFX 2 follow TalonFX 1    
         motor2.setControl(new Follower(motor1.getDeviceID(), MotorAlignmentValue.Aligned));
-        StartingPos = currentAngle();
+        StartingPos = getCurrentAngle();
     }
 
-    public void Climb(){
-        motor1.setControl(positionRequest.withPosition(Revolutions));
+    public Command Climb(){
+        motor1.setControl(positionRequest.withPosition(Constants.target));
+                return null;
     }
-    public void retractClimb(){
+    public Command retractClimb(){
         motor1.setControl(positionRequest.withPosition(StartingPos/360));
+                return null;
     }
-    public void extendClimb(){
-        motor1.setControl(positionRequest.withPosition(Fullextend));
+    public Command extendClimb(){
+        motor1.setControl(positionRequest.withPosition(Constants.fullExtend));
+                return null;
     }
     public Boolean isAtTarget(){
-        return (Math.abs(currentAngle()-Revolutions*360/25) <= Constants.tolerance);
+        return (Math.abs(getCurrentAngle()-Constants.target*360/25) <= Constants.tolerance);
     }
-    private Double currentAngle(){
+    private Double getCurrentAngle(){
         return  motor1.getPosition().getValueAsDouble() * (1/25) * 360;
     }
-    
+    public Command StopMotors(){
+        motor1.stopMotor();
+        motor2.stopMotor();
+        return null;
+    }
+    public Command setState(String state){
+        return null;
+    }
+    public Command setStateBased(boolean using){
+        return null;
+    }
 }
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,4 +2,9 @@ package frc.robot;
 
 public class Constants {
     public static double tolerance = 5;
+    public static int Motor1ID = 1;
+    public static int Motor2ID = 2;
+    public static double target = 15.0;
+    public static double fullExtend = 20;
+    public String state;
 }
