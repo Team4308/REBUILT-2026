8c646a608c52d61aa5e3808110eed50ba710b546
C0DE-Z <melonicholas1113@gmail.com>
2026-02-16 00:07:06 -0500

    Update robot con

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -7,10 +7,11 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.Util.TrajectoryCalculations;
+import frc.robot.subsystems.LedSubsystem;
 
 public class RobotContainer {
   private final TrajectoryCalculations trajectoryCalculations = new TrajectoryCalculations();
-
+  private final LedSubsystem ledSubsystem = new LedSubsystem();
   public RobotContainer() {
     
     configureBindings();
