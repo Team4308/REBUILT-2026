19acc7a2a605b6a36aa65ac707a81b086a9bf2ce
Orange-Panda-ui <riyavishnu11@gmail.com>
2026-02-17 16:35:02 -0500

    Endgame Commands

---
+++ src/main/java/frc/robot/commands/EndgameCommands.java
@@ -0,0 +1,23 @@
+package frc.robot.commands;
+
+public class EndgameCommands { /*PrepareClimbCommand // this will move the climber to the prepared position and retract the intake
+
+ClimbCommand // This will retract the climber to the climb position assuming it is already extended
+
+ToggleClimbCommand 
+If the climber is retracted, prepare
+If the climber is prepared, climb
+If the climber is climbed, move it to prepare
+
+resetclimberCommand
+Just move the climber to home position
+
+ReleaseClimbCommand // This will move the climber to the top, then wait some amount of time(2 seconds for now), then automatically retract to the bottom.
+
+AutoAlignLeftThenClimbCommand // This will retract the intake, move climbers up, then drive to the right/left side, then do the climbing stuff
+You need to use drivebase.drivetopose() for that.
+
+AutoAlignRightThenClimbCommand 
+ */
+    
+}
