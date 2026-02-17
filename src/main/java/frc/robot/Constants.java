ce3252c98f53242be7772f295a975c7409687f71
Junyang <junyangpu1@gmail.com>
2026-02-17 16:28:31 -0500

    climber subsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,11 @@
 package frc.robot;
 
 public class Constants {
-    public double tolerance = 2;
+    public static class ClimberConstants{
+        public static double tolerance = 2;
+        public static int ClimberMotor1 = 1;
+        public static int ClimberMotor2 = 2;
+        public static double RotationsToHighTarget = 20;
+        public static double RotationsToTarget = 15;
+    }
 }
