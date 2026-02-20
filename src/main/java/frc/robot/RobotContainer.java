5cec888c4eb4444e359cf227336f30c864dcf17e
Maaz Siddiqui <maazs8856@gmail.com>
2026-02-20 15:52:20 -0500

    Added bindings to Robot Container

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,22 +4,56 @@
 
 package frc.robot;
 
+import ca.team4308.absolutelib.control.XBoxWrapper;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Util.TrajectoryCalculations;
+import frc.robot.subsystems.HoodSubsystem;
 
 public class RobotContainer {
+
+  public HoodSubsystem m_HoodSubsystem;
+
+  private final XBoxWrapper m_driverController = new XBoxWrapper(Constants.Hood.kDriverControllerPort);
+
   public RobotContainer() {
+    m_HoodSubsystem = new HoodSubsystem();
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+      // Toggle shoot/rest
+      m_driverController.A.onTrue(
+        Commands.runOnce(() -> {
+            m_HoodSubsystem.setState(
+                m_HoodSubsystem.getState() == HoodSubsystem.RobotState.SHOOT
+                    ? HoodSubsystem.RobotState.REST
+                    : HoodSubsystem.RobotState.SHOOT
+            );
+        })
+      );
+
+      // Pass presets
+      m_driverController.X.onTrue(
+          Commands.runOnce(() -> m_HoodSubsystem.setState(HoodSubsystem.RobotState.PASS_LEFT))
+      );
+
+      m_driverController.Y.onTrue(
+          Commands.runOnce(() -> m_HoodSubsystem.setState(HoodSubsystem.RobotState.PASS_RIGHT))
+      );
+
+      // Reset hood
+      m_driverController.B.onTrue(
+          m_HoodSubsystem.resetHoodCommand()
+      );
+
+      // Emergency stop
+      m_driverController.Back.onTrue(
+          Commands.runOnce(() -> m_HoodSubsystem.stopMotors())
+      );
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
 
-  public void robotPeriodic() {
-    new TrajectoryCalculations().updateShot();
-  }
 }
