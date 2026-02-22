8448d9bc61fe7ee44a72718204387869629b0059
Orange-Panda-ui <riyavishnu11@gmail.com>
2026-02-21 20:43:59 -0500

    climb

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,22 +4,46 @@
 
 package frc.robot;
 
+import java.io.File;
+import java.util.jar.Attributes.Name;
+
+import com.ctre.phoenix.platform.can.AutocacheState;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+
+import ca.team4308.absolutelib.control.XBoxWrapper;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.Filesystem;
+
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.Util.TrajectoryCalculations;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.Constants.Driver;
+
+import frc.robot.subsystems.indexer.IndexerSubsystem;
 
 public class RobotContainer {
+
+  private final XBoxWrapper driver = new XBoxWrapper(Ports.Joysticks.DRIVER);
+
+  private final IndexerSubsystem indexer = new IndexerSubsystem();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    driver.A.onTrue(
+        Commands.runOnce(indexer::runMotors, indexer)
+  );
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
-  }
-
-  public void robotPeriodic() {
-    new TrajectoryCalculations().updateShot();
   }
 }
