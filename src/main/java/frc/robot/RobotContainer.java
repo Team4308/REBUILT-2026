004ef2520bd89ed27f23bcd1316648c5b1943e41
Owen Sun <owen31474@gmail.com>
2026-02-22 11:31:09 -0500

    added button system, still rudimentary and i think im missing something

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,24 +4,55 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.Subsystems.turretSubsystem;
+
+import ca.team4308.absolutelib.control.XBoxWrapper;
+
 import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.TimedRobot;
+
+import frc.robot.RobotState;
+
 public class RobotContainer {
-public turretSubsystem m_turret = new turretSubsystem();
+  public turretSubsystem m_turret = new turretSubsystem();
 
 
   public RobotContainer() {
     configureBindings();
-
   }
 
-  private void configureBindings() {
-    //add button bindings here
-  } 
+    private final Joystick driver = new Joystick(0);
 
+    double x = driver.getRawAxis(0); 
+    private final JoystickButton aButton = new JoystickButton(driver, 1);
+    private final JoystickButton bButton = new JoystickButton(driver, 2);
+    private final JoystickButton xButton = new JoystickButton(driver, 3);
+  
+  private void configureBindings() {
+
+  aButton.onTrue(
+      new InstantCommand(() -> m_turret.switchState(RobotState.aimAtHub))
+  );
+
+  bButton.onTrue(
+      new InstantCommand(() -> m_turret.switchState(RobotState.aimAtPassingZone))
+  );
+
+  xButton.onTrue(
+      new InstantCommand(() -> m_turret.switchState(RobotState.defaultTurret))
+  );
+  }
+  
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
   }
