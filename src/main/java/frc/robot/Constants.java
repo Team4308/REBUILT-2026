79e29d42e5a603b1b2e75afec1126621f2ba6d1b
C0DE-Z <melonicholas1113@gmail.com>
2026-02-23 09:01:30 -0500

    Added Driver View Leds, updated venderops also

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -25,5 +25,7 @@ public class Constants {
     public static final class Leds {
         public static final int LED_PORT = 0;
         public static final int LED_LENGTH = 60;
+        public static final int[] startIndexes = new int[] {0, 60, 120, 180, 240}; // Front, Back, Left, Right, UnderGlow
+        public static final int[] viewAngles = new int[] {0, 90, 180, 270}; // Front, Back, Left, Right (in Degrees from facing the front of the robot)
     }
 }
--- src/main/java/frc/robot/subsystems/LedSubsystem.java
+++ src/main/java/frc/robot/subsystems/LedSubsystem.java
@@ -1,11 +1,13 @@
 package frc.robot.subsystems;
 
-import com.google.flatbuffers.Constants;
+import java.util.Map;
 
 import ca.team4308.absolutelib.leds.AddressableLEDBufferView;
 import ca.team4308.absolutelib.leds.LEDPattern;
 import ca.team4308.absolutelib.leds.Patterns;
 import ca.team4308.absolutelib.wrapper.AbsoluteSubsystem;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry3d;
 import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
@@ -17,11 +19,24 @@ import frc.robot.StateManager;
 public class LedSubsystem extends AbsoluteSubsystem {
     private final AddressableLED led;
     private final AddressableLEDBuffer buffer;
-    private final AddressableLEDBufferView view;
+    private final AddressableLEDBufferView view; // Front
+    private final AddressableLEDBufferView viewBack; // Back
+    private final AddressableLEDBufferView viewLeft; // Left
+    private final AddressableLEDBufferView viewRight; // Right
+    private final AddressableLEDBufferView underGlow; // Underneath the bot (In brain pan?)
+    private Map<Integer, AddressableLEDBufferView> allViews = new java.util.HashMap<>();
+    private Pose2d[] DriverSationCords = {
+        // Blue Alliance driver station locations 
+        new Pose2d(0, 7, new edu.wpi.first.math.geometry.Rotation2d()),
+        new Pose2d(0, 4, new edu.wpi.first.math.geometry.Rotation2d()),
+        new Pose2d(0, 1, new edu.wpi.first.math.geometry.Rotation2d()),
+    };
+    
 
     private LEDPattern currentPattern;
+    private LEDPattern driverPattern; /** Data for the driver **/
     private String currentPatternName = "Idle";
-
+    private String driverPatternName = "Idle";
     private static final int LED_PORT = Leds.LED_PORT;
     private static final int LED_LENGTH = Leds.LED_LENGTH;
 
@@ -32,16 +47,67 @@ public class LedSubsystem extends AbsoluteSubsystem {
         led.setLength(LED_LENGTH);
         led.setData(buffer);
         led.start();
+        view = new AddressableLEDBufferView(buffer, Leds.startIndexes[0], LED_LENGTH);
+        viewBack = new AddressableLEDBufferView(buffer, Leds.startIndexes[1], LED_LENGTH);
+        viewLeft = new AddressableLEDBufferView(buffer, Leds.startIndexes[2], LED_LENGTH);
+        viewRight = new AddressableLEDBufferView(buffer, Leds.startIndexes[3], LED_LENGTH);
+        underGlow = new AddressableLEDBufferView(buffer, Leds.startIndexes[4], LED_LENGTH);
+        allViews.put(Leds.viewAngles[0], view);
+        allViews.put(Leds.viewAngles[1], viewBack);
+        allViews.put(Leds.viewAngles[2], viewLeft);
+        allViews.put(Leds.viewAngles[3], viewRight);
 
-        view = new AddressableLEDBufferView(buffer, 0, LED_LENGTH);
+        currentPattern = Patterns.defaultScrollingIdle();
+        driverPattern = Patterns.defaultScrollingIdle();
+    }
 
-        currentPattern = Patterns.idle();
+
+    public AddressableLEDBufferView getViewFacingDriver(SwerveDriveOdometry3d odometry) {
+        double robotX = odometry.getPoseMeters().getX();
+        double robotY = odometry.getPoseMeters().getY();
+        double robotAngle = odometry.getPoseMeters().getRotation().toRotation2d().getDegrees();
+        
+        double dsX = 0.0;
+        double dsY = 0.0;
+
+        var location = DriverStation.getLocation();
+        if (location.isPresent() && location.getAsInt() >= 1 && location.getAsInt() <= 3) {
+            Pose2d dsPose = DriverSationCords[location.getAsInt() - 1];
+            dsX = dsPose.getX();
+            dsY = dsPose.getY();
+        }
+
+        var alliance = DriverStation.getAlliance();
+        if (alliance.isPresent() && alliance.get() == DriverStation.Alliance.Red) {
+            dsX = 16.541; // Might need to adjust 
+        }
+
+        double driverStationAngle = Math.toDegrees(Math.atan2(dsY - robotY, dsX - robotX));
+
+        double angleToDriver = (driverStationAngle - robotAngle) % 360.0;
+        if (angleToDriver < 0) {
+            angleToDriver += 360.0;
+        }
+
+        int nearestViewAngle = (int) Math.round(angleToDriver / 90.0) * 90;
+        if (nearestViewAngle == 360) {
+            nearestViewAngle = 0;
+        }
+
+        return allViews.get(nearestViewAngle);
     }
 
     @Override
     public void periodic() {
         if (currentPattern != null) {
-            currentPattern.applyTo(view);
+            for (AddressableLEDBufferView view : allViews.values()) {
+                // Uncomment code when we have swerve added
+               // if (view == getViewFacingDriver(StateManager.getInstance().getSwerveOdometry())) {
+                //   currentPattern.applyTo(view);
+                //}
+                currentPattern.applyTo(view);
+            }
+            Patterns.defaultScrollingIdle().applyTo(underGlow);
         }
         led.setData(buffer);
     }
@@ -49,47 +115,45 @@ public class LedSubsystem extends AbsoluteSubsystem {
     public void setIdle() {
         currentPattern = Patterns.defaultScrollingIdle();
         currentPatternName = "Idle";
+
+        driverPattern = Patterns.defaultScrollingIdle();
+        driverPatternName = "Idle";
+
     }
 
+    /**
+     * Sets the Drivers LED pattern to error      
+     */
     public void setError() {
-        currentPattern = Patterns.error();
-        currentPatternName = "Error";
+        driverPattern = Patterns.error();
+        driverPatternName = "Error";
     }
 
+        /**
+     * Sets the Drivers LED pattern to success      
+     */
     public void setSuccess() {
-        currentPattern = Patterns.success();
-        currentPatternName = "Success";
+        driverPattern = Patterns.success();
+        driverPatternName = "Success";
     }
-
+    /**
+     * Sets the Drivers LED pattern to warning      
+     */
     public void setWarning() {
-        currentPattern = Patterns.warning();
-        currentPatternName = "Warning";
+        driverPattern = Patterns.warning();
+        driverPatternName = "Warning";
+    }
+    /**
+     * Sets the Drivers LED pattern to loading      
+     * @param speed the speed of the loading pattern, where 1.0 is normal speed
+     */
+    public void setWarning(double speed) {
+        driverPattern = Patterns.createBreathingPattern(Color.kYellow, speed);
+        driverPatternName = "Warning";
     }
 
-    public void setRainbow() {
-        currentPattern = Patterns.rainbowChase();
-        currentPatternName = "Rainbow";
-    }
 
-    public void setAlliance() {
-        currentPattern = Patterns.getAlliancePattern();
-        currentPatternName = "Alliance";
-    }
 
-    public void setAllianceBreathing() {
-        currentPattern = Patterns.getAllianceBreathing(2.0);
-        currentPatternName = "Alliance Breathing";
-    }
-
-    public void setSolidColor(Color color) {
-        currentPattern = Patterns.createSolidPattern(color);
-        currentPatternName = "Solid";
-    }
-
-    public void setProgress(double progress, Color color) {
-        currentPattern = Patterns.createProgressPattern(color, progress);
-        currentPatternName = "Progress";
-    }
 
     public void setChasingDot(Color color) {
         currentPattern = Patterns.chasingDot(color);
@@ -103,43 +167,21 @@ public class LedSubsystem extends AbsoluteSubsystem {
         }
 
         switch (state) {
-            case Home:
-                currentPattern = Patterns.getAllianceBreathing(3.0);
+            case Home :
+                currentPattern = Patterns.getAllianceBreathing(2.0);
                 break;
 
-            case ActiveTeleopAllianceZone:
-                currentPattern = Patterns.createRainbowPattern(LED_LENGTH, 4, 1.0, 1.0);
-                break;
-
-            case ActiveTeleopNeutralZone:
-                currentPattern = Patterns.chasingDot(Color.kGreen);
-                break;
-
-            case ActiveTeleopOpponentZone:
-                currentPattern = Patterns.createBlinkingPattern(Color.kOrangeRed, 0.15);
-                break;
-
-            case InactiveTeleopAllianceZone:
-                currentPattern = Patterns.scrollingIdle(Color.kCyan, 2);
-                break;
-
-            case InactiveTeleopNeutralZone:
-                currentPattern = Patterns.createBreathingPattern(Color.kYellow, 1.5);
-                break;
-
-            case InactiveTeleopOpponentZone:
-                currentPattern = Patterns.chasingDot(Color.kMagenta);
-                break;
-
-            case EndgameTeleopAllianceZone:
-                    currentPattern = Patterns.createBlinkingPattern(Color.kRed, DriverStation.getMatchTime() / 30.0);
-                    break;
-            case EndgameTeleopNeutralZone:
-                        currentPattern = Patterns.createBlinkingPattern(Color.kRed, DriverStation.getMatchTime() / 30.0);
-                    break;
-            case EndgameTeleopOpponentZone:
+            case ActiveTeleopAllianceZone, ActiveTeleopOpponentZone, ActiveTeleopNeutralZone:
                 currentPattern = Patterns.altF4();
                 break;
+
+            case InactiveTeleopAllianceZone,InactiveTeleopNeutralZone, InactiveTeleopOpponentZone:
+                currentPattern = Patterns.defaultScrollingIdle();
+                break;
+
+            case EndgameTeleopAllianceZone,EndgameTeleopNeutralZone, EndgameTeleopOpponentZone:
+                    currentPattern = Patterns.createBlinkingPattern(Color.kRed, DriverStation.getMatchTime() / 30.0);
+            break;
 
             default:
                 currentPattern = Patterns.defaultScrollingIdle();
@@ -150,15 +192,11 @@ public class LedSubsystem extends AbsoluteSubsystem {
     }
 
     public String getCurrentPatternName() {
-        return currentPatternName;
+        return currentPatternName + ", " + driverPatternName;
     }
 
     @Override
     public Sendable log() {
-        return builder -> {
-            builder.setSmartDashboardType("LEDs");
-            builder.addStringProperty("CurrentPattern", () -> currentPatternName, null);
-            builder.addIntegerProperty("LEDCount", () -> LED_LENGTH, null);
-        };
+        return null;
     }
 }
