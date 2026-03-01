801469050e643f663ffe6771b98c444a7b2815d8
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2026-02-28 21:17:03 -0500

    rename some stuff and cleanup

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -5,20 +5,5 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 
 public class Constants {
-    public static final class Hood {
-        public static final int HoodMotor = 10; // CAN ID for the hood motor
-        public static final double kPassingAngle = 30.0; //angle for passing zone
-        public static final double kToleranceDegrees = 2.0; // Tolerance for position control
-        public static final double TOTAL_GEAR_RATIO = 50.0 * (42.0 / 12.0); // Gear ratio
-        public static final double FORWARD_SOFT_LIMIT_ANGLE = 82.0;
-        public static final double REVERSE_SOFT_LIMIT_ANGLE = 47.0;
-        public static final double ampThreshold = 20;
-        public static final double max_accel = 1000;
-        public static final double max_velocity = 500;
-        public final static ArmFeedforward feedforward = new ArmFeedforward(0, 0.28, 0.0155, 0.01);
-        public final static ProfiledPIDController pidController = new ProfiledPIDController(
-        0.06, 0.0, 0.0, 
-        new TrapezoidProfile.Constraints(Constants.Hood.max_velocity, Constants.Hood.max_accel)
-        );
-    }
+   
 }
