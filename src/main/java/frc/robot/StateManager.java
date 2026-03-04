edb330a2716559364c2cb1f1338fca6ce5e1e878
Maaz Siddiqui <maazs8856@gmail.com>
2026-03-04 16:32:53 -0500

    added the last logic state

--- src/main/java/frc/robot/StateManager.java
+++ src/main/java/frc/robot/StateManager.java
@@ -7,8 +7,8 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
-public class StateManager{
-     public enum State {
+public class StateManager {
+    public enum State {
         ActiveTeleopAllianceZoneResting,
         ActiveTeleopAllianceZoneShooting,
         ActiveTeleopAllianceZoneIntaking,
@@ -33,14 +33,17 @@ public class StateManager{
         ClimbPrepareRight,
         ClimbedUp,
         Home
-    } 
-    private final XboxController m_StateController = new XboxController(Constants.State.kStateManagerPort);
-    private final XboxController m_fuelStatus = new XboxController(2);
+    }
+
+    private final XboxController m_primaryController = new XboxController(Constants.State.kPrimaryControllerPort);
+    private final XboxController m_secondaryController = new XboxController(Constants.State.kSecondaryControllerPort);
     SwerveSubsystem m_swerveSubsystem = new SwerveSubsystem();
     private State currentState = State.Home;
     boolean isIntaking = false;
     boolean active = false;
     boolean isShooting = false;
+    boolean passing = false;
+
     public void setState(State state) {
         this.currentState = state;
     }
@@ -48,7 +51,6 @@ public class StateManager{
     public State getState(State state) {
         return state;
     }
-
 
     public StateManager(CommandXboxController controller) {
         configurePrimaryBindings(controller);
@@ -58,61 +60,120 @@ public class StateManager{
     private void configurePrimaryBindings(CommandXboxController controller) {
 
         controller.a().onTrue(
-            new InstantCommand(() -> {
-                isShooting = (isShooting) ? false : true;
-                if(m_swerveSubsystem.getFieldLocation() == "AllianceZone"){
-                    if(active){
-                        if(isIntaking){
-                            setState(State.ActiveTeleopAllianceZoneShootingIntaking);
+                new InstantCommand(() -> {
+                    isShooting = (isShooting) ? false : true;
+                    if (isShooting) {
+                        if (m_swerveSubsystem.getFieldLocation() == "AllianceZone") {
+                            if (active) {
+                                if (isIntaking) {
+                                    setState(State.ActiveTeleopAllianceZoneShootingIntaking);
+                                } else {
+                                    setState(State.ActiveTeleopAllianceZoneShooting);
+                                }
+                            } else {
+                                setState(State.InactiveTeleopAllianceZoneIntaking);
+                            }
+
+                        } else if (m_swerveSubsystem.getFieldLocation() == "NeutralZone") {
+                            if (active) {
+                                if (isIntaking && passing) {
+                                    setState(State.ActiveTeleopNeutralZonePassingIntaking);
+                                } else if (isIntaking) {
+                                    setState(State.ActiveTeleopNeutralZoneIntaking);
+                                } else if (passing) {
+                                    setState(State.ActiveTeleopNeutralZonePassing);
+                                } else {
+                                    setState(State.ActiveTeleopNeutralZoneResting);
+                                }
+                            } else {
+                                if (isIntaking && passing) {
+                                    setState(State.InactiveTeleopNeutralZonePassingIntaking);
+                                } else {
+                                    setState(State.InactiveTeleopNeutralZoneIntaking);
+                                }
+                            }
+                        } else if (m_swerveSubsystem.getFieldLocation() == "OpponentZone") {
+                            if (active) {
+
+                                if (isIntaking && passing) {
+                                    setState(State.ActiveTeleopOpponentZonePassingIntaking);
+                                } else if (isIntaking) {
+                                    setState(State.ActiveTeleopOpponentZoneIntaking);
+                                } else if (passing) {
+                                    setState(State.ActiveTeleopOpponentZonePassing);
+                                } else {
+                                    setState(State.ActiveTeleopOpponentZoneResting);
+                                }
+                            } else {
+                                if (isIntaking && passing) {
+                                    setState(State.InactiveTeleopOpponentlZonePassingIntaking);
+                                } else {
+                                    setState(State.InactiveTeleopOpponentZoneIntaking);
+                                }
+                            }
+                        }
+
+                    } else if (active) {
+                        if (m_swerveSubsystem.getFieldLocation() == "AllianceZone") {
+                            setState(State.ActiveTeleopAllianceZoneResting);
+                        } else if (m_swerveSubsystem.getFieldLocation() == "NeutralZone") {
+                            setState(State.ActiveTeleopNeutralZoneResting);
+                        } else if (m_swerveSubsystem.getFieldLocation() == "OpponentZone") {
+                            setState(State.ActiveTeleopOpponentZoneResting);
+                        }
+
+                    } else if (!active) {
+                        if (m_swerveSubsystem.getFieldLocation() == "AllianceZone") {
+                            setState(State.InactiveTeleopAllianceZoneResting);
+
+                        } else if (m_swerveSubsystem.getFieldLocation() == "NeutralZone") {
+                            setState(State.InactiveTeleopNeutralZoneResting);
+                        } else if (m_swerveSubsystem.getFieldLocation() == "OpponentZone") {
+                            setState(State.InactiveTeleopOpponentZoneResting);
                         }
                     }
-                    
-                }else if(m_swerveSubsystem.getFieldLocation() == "NeutralZone"){
-                    setState(State.ActiveTeleopAllianceZoneShootingIntaking);
-                }
-                else if(m_swerveSubsystem.getFieldLocation() == "OpponentZone"){
-                    setState(State.ActiveTeleopOpponentZoneIntaking);
-                }
-            })
-        );
+
+                }));
 
         controller.b().onTrue(
-            new InstantCommand(() -> setState())
-        );
+                new InstantCommand(() -> setState()));
 
         controller.x().onTrue(
-            new InstantCommand(() -> setState())
-        );
+                new InstantCommand(() -> setState()));
     }
 
-    /* Secondary controller bindings, buttons are toggling and determine:
-    - active/inactive
-    - if the hopper is full
-    - Shooting or not */
-    private void configureSecondaryBindings(CommandXboxController controller){
+    /*
+     * Secondary controller bindings, buttons are toggles and determine:
+     * - active/inactive
+     * - if the hopper is full
+     * - Shooting or not
+     * - Passing or not
+     */
+
+    private void configureSecondaryBindings(CommandXboxController controller) {
         // Intake Toggle
         controller.a().onTrue(
-            new InstantCommand(()->{
-                isIntaking = (isIntaking) ? false : true;
-            })
-        );
+                new InstantCommand(() -> {
+                    isIntaking = (isIntaking) ? false : true;
+                }));
         // Active/Inactive toggle
+        controller.b().onTrue(
+                new InstantCommand(() -> {
+                    active = (active) ? false : true;
+                }));
 
-        // HUB is active
+        // Passing toggle
         controller.x().onTrue(
-            new InstantCommand(()->{
-                active = (active)? false: true;
-            })
-        );
-
+                new InstantCommand(() -> {
+                    passing = (passing) ? false : true;
+                }));
 
     }
 
+    public void periodic() {
 
-    public void periodic(){
+        switch (currentState) {
 
-        switch(currentState){
-            
             case ActiveTeleopAllianceZoneResting:
             case ActiveTeleopAllianceZoneShooting:
             case ActiveTeleopAllianceZoneIntaking:
@@ -139,6 +200,6 @@ public class StateManager{
             case Home:
 
         }
-    }  
-    
+    }
+
 }
