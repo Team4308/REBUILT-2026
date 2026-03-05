06794363f4831d402ad62c6bb6511b4c92457d68
Maaz Siddiqui <maazs8856@gmail.com>
2026-03-05 00:15:05 -0500

    Cleaned up file structure, removed most java files (Robot, RobotContainer, Main, etc...) kept only neccessary files StateManager && Constants  to avoid any merge conflicts (also removed Autons & Paths)

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,31 +1,6 @@
 package frc.robot;
 
-import edu.wpi.first.math.controller.ArmFeedforward;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Translation3d;
-import edu.wpi.first.math.trajectory.TrapezoidProfile;
-
 public class Constants {
-    public static final class Hood {
-        public static final int HoodMotor = 10; // CAN ID for the hood motor
-        public static final double kToleranceDegrees = 2.0; // Tolerance for position control
-        public static final double TOTAL_GEAR_RATIO = 60.2; // Gear ratio
-        public static final double FORWARD_SOFT_LIMIT_ANGLE = 52.5;
-        public static final double REVERSE_SOFT_LIMIT_ANGLE = 7.5;
-        public static final double ampThreshold = 20;
-        public static final double max_accel = 1000;
-        public static final double max_velocity = 500;
-        public final static ArmFeedforward feedforward = new ArmFeedforward(0, 0, 0, 0.01);
-        public final static ProfiledPIDController pidController = new ProfiledPIDController(
-                1, 0.0, 0.0,
-                new TrapezoidProfile.Constraints(Constants.Hood.max_velocity, Constants.Hood.max_accel));
-
-        public static final Translation3d HUB = new Translation3d(4.0, 0.0, 2.1);
-        public static final Translation3d kPASS_LEFT = new Translation3d(4.0, 0.5, 2.1);
-        public static final Translation3d kPASS_RIGHT = new Translation3d(4.0, -0.5, 2.1);
-        public static final int kDriverControllerPort = 0;
-
-    }
     public static final class State{
         public static final int kPrimaryControllerPort = 1;
         public static final int kSecondaryControllerPort = 2;
