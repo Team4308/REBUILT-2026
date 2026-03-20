e236b3c5002840aba0b6a00fb5414c8b17837ab3
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2026-03-20 13:21:58 -0400

    named command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -3,6 +3,7 @@ package frc.robot;
 import java.io.File;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
 
 import ca.team4308.absolutelib.control.RazerWrapper;
 import edu.wpi.first.math.geometry.Pose2d;
@@ -110,6 +111,7 @@ public class RobotContainer {
         }
 
         public void configureNamedCommands() {
+                NamedCommands.registerCommand("Shoot", new ShootCommand(m_IndexerSubsystem));
         }
 
         public Command getTestCommand() {
