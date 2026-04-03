fcde6a4250dbd9f2e07089a3199e3cbc7032c432
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2026-04-02 21:17:51 -0400

    b

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -37,8 +37,6 @@ public class Robot extends LoggedRobot {
                     // be added.
 
     m_robotContainer = new RobotContainer();
-
-    PathfindingCommand.warmupCommand().schedule();
   }
 
   @Override
